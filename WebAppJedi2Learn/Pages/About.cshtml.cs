29fd60b81dfbc25b318a1c096bce4624f9ccd707
CodeOrNotToCode <CodeOrNotToCode@outlook.com>
2018-06-11 22:28:26 -0500

    Add project files.

---
+++ WebAppJedi2Learn/Pages/About.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppJedi2Learn.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        /*
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+        */
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+
+    }
+}
